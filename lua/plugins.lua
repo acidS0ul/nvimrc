af13e636e1b6daec0ebf5a0d01b9aae39d7cc0c8
Denis Semenov <37097033+acidS0ul@users.noreply.github.com>
2024-12-05 23:07:45 +0300

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,8 +4,8 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
-
-
+    use "rebelot/kanagawa.nvim"
+    vim.cmd("colorscheme kanagawa-dragon")
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.8',
         -- or                       , branch = '0.1.x',
