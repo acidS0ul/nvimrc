861efd7f751a75c93ec704beb7f7aed5e848bb83
Denis Semenov <37097033+acidS0ul@users.noreply.github.com>
2025-02-11 09:15:01 +0300

    add new keymap

--- lua/remap.lua
+++ lua/remap.lua
@@ -11,3 +11,9 @@ map('', '<right>', ':echoe "Use hjkl"<CR>', {noremap = true, silent = false})
 vim.api.nvim_set_keymap('n', '<Tab>', ':tabnext<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader><Tab>', ':tabnew<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<S-Tab>', ':tabprevious<CR>', { noremap = true, silent = true })
+-- Reg
+vim.api.nvim_set_keymap('n', '<leader>yiw', '"+yiw', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>yw', '"+yw', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>y',  '"+y',   { noremap = true, silent = true })
+-- Diff
+vim.api.nvim_set_keymap('n', '<Leader>dg', ':diffget<CR>', { noremap = true, silent = true })
