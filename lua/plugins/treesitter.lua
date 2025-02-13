9fdaa3faf43d58f426ef8fefd257c73f4c923c0a
acidS0ul <dvfhell@gmail.com>
2025-02-13 23:11:38 +0300

    update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,13 @@
-local M = {
-    "nvim-treesitter/nvim-treesitter",
+return {
+    "nvim-treesitter/nvim-treesitter",    
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "markdown", "markdown_inline" },
+    sync_install = false, 
+    highlight = {
+        enable = true,              
+        additional_vim_regex_highlighting = false,
+    },
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
-}
 
-return { M }
+}
