725af32114a3089426a2a06b2b3e61d8138e5177
acidS0ul <dvfhell@gmail.com>
2025-02-23 15:30:20 +0300

    add switch theme

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -1,1 +1,7 @@
-vim.cmd('colorscheme github_dark')
+local current_time = os.date("%H:%M")
+
+if current_time < "17:00" then
+    vim.cmd('colorscheme github_light_default')
+else
+    vim.cmd('colorscheme github_dark_default')
+end
