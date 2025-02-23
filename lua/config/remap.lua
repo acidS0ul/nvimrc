21a791d583a49582702636508c09abc500e9b308
acidS0ul <dvfhell@gmail.com>
2025-02-23 22:05:34 +0300

    update remaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -3,20 +3,22 @@ local default_opts = {noremap = true, silent = true}
 -- General
 vim.g.mapleader = " "
 -- Movement
-map('', '<up>', ':echoe "Use hjkl"<CR>', {noremap = true, silent = false})
-map('', '<down>', ':echoe "Use hjkl"<CR>', {noremap = true, silent = false})
-map('', '<left>', ':echoe "Use hjkl"<CR>', {noremap = true, silent = false})
-map('', '<right>', ':echoe "Use hjkl"<CR>', {noremap = true, silent = false})
+map('', '<up>',     ':echoe "Use hjkl"<CR>', {noremap = true, silent = false})
+map('', '<down>',   ':echoe "Use hjkl"<CR>', {noremap = true, silent = false})
+map('', '<left>',   ':echoe "Use hjkl"<CR>', {noremap = true, silent = false})
+map('', '<right>',  ':echoe "Use hjkl"<CR>', {noremap = true, silent = false})
 -- Tabs
 vim.api.nvim_set_keymap('n', '<Tab>', ':tabnext<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader><Tab>', ':tabnew<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<S-Tab>', ':tabprevious<CR>', { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap('n', '<Leader>yy', '"+yy<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>yw', '"+yw<CR>', { noremap = true, silent = true })
+-- Reg
+vim.api.nvim_set_keymap('n', '<Leader>yy',  '"+yy<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>yw',  '"+yw<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>yiw', '"+yiw<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<Leader>y', '"+y<CR>', { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap('v', '<Leader>y',   '"+y<CR>', { noremap = true, silent = true })
+-- Diff
+vim.api.nvim_set_keymap('n', '<Leader>dg', ':diffget<CR>', { noremap = true, silent = true })
+-- Telekasten
 vim.keymap.set("n", "<leader>tf", "<cmd>Telekasten find_notes<CR>")
 vim.keymap.set("n", "<leader>tg", "<cmd>Telekasten search_notes<CR>")
 vim.keymap.set("n", "<leader>td", "<cmd>Telekasten goto_today<CR>")
