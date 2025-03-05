118d6e2ba7db0569cd63b681911f2f4f4008b675
acidS0ul <dvfhell@gmail.com>
2025-03-05 22:27:52 +0300

    telescope: add ignore patterns

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,17 @@
 return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.8',
 	dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+        defauts = {
+            file_ignore_patterns = {
+                "%.git",
+                "%.log",
+                "%.tmp",
+                "%.patch",
+                "%tags",
+            },
+        },
+    },
 
     config = function()
         local builtin = require('telescope.builtin')
