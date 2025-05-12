afba9dfbf9ee8d99c1eb09b697a3ad08ab31a821
Denis Semenov <37097033+acidS0ul@users.noreply.github.com>
2025-05-12 09:48:29 +0300

    theme.lua: fix theme switching

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -1,6 +1,6 @@
-local current_time = os.date("%H:%M")
+local hours = tonumber(os.date("%H"))
 
-if current_time < "17:00" and current_time > "7:00" then
+if (hours < 17) and (hours > 7) then
     vim.cmd('colorscheme github_light_default')
 else
     vim.cmd('colorscheme github_dark_default')
