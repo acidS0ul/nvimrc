0165b46fe791df35eebf25669f985ed83b44e37b
acidS0ul <dvfhell@gmail.com>
2025-09-02 22:19:33 +0300

    theme.lua: change dark colorsheme

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -3,5 +3,5 @@ local hours = tonumber(os.date("%H"))
 if (hours < 17) and (hours > 7) then
     vim.cmd('colorscheme github_light_default')
 else
-    vim.cmd('colorscheme github_dark_default')
+    vim.cmd('colorscheme github_dark_high_contrast')
 end
